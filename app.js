c50cede58e4f2c4ba4a8b8ce41003f6950dafe22
Connie <constancecrawford87@gmail.com>
2021-10-16 17:20:30 -0400

    es6

---
+++ app.js
@@ -0,0 +1,39 @@
+//Regular function
+function addTwo(a,b){
+    console.log(a + b);
+}
+
+//Arrow function
+const addTwo = (a,b) => {
+    console.log(a + b);
+}
+
+//Regular function 
+function minusTwo(a,b){
+    console.log(a - b);
+}
+
+// Arrow function
+const minusTwo = (a,b) => {
+    console.log(a - b);
+}
+
+// Regular function
+function multiplyTwo(a,b){
+    console.log(a*b);
+}
+
+// Arrow function
+const multiplyTwo = (a,b) => {
+    console.log(a * b);
+}
+
+// Regular function
+function divideTwo(a,b){
+    console.log(a/b);
+}
+
+// Arrow function
+const divideTwo = (a,b) => {
+    console.log(a/b);
+}
